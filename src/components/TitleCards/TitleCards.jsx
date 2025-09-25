1103da5664b49e00adeb787aceeacd4514fd4595
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-25 15:32:41 -0500

    title cards and css

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
+
+const cardsRef = useRef();
 
 const TitleCards = () => {
   return (
     <div className='title-cards'>
       <h2>Popular on Netflix</h2>
-      <div className="card-list">
+      <div className="card-list" ref={cardsRef}>
         {cards_data.map((card, index) => {
             return <div className="card" key={index}>
                 <img src={card.image} alt="" />
