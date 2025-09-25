d6e8259f1ae1644aae56058d51ff44ec27cbb5ab
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-25 15:51:23 -0500

    title cards titles

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -4,7 +4,7 @@ import cards_data from '../../assets/cards/Cards_data'
 
 
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
     const cardsRef = useRef();
 
 const handleWheel = (event) => {
@@ -17,7 +17,7 @@ useEffect(() => {
 }, [])
   return (
     <div className='title-cards'>
-      <h2>Popular on Netflix</h2>
+      <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
         {cards_data.map((card, index) => {
             return <div className="card" key={index}>
