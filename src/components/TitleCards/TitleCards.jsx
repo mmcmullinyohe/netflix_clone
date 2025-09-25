9e011fb7a9a197d6a6b6981e9a2d7f6638319cf1
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-25 15:37:02 -0500

    title card functions

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,20 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
-const cardsRef = useRef();
+
 
 const TitleCards = () => {
+    const cardsRef = useRef();
+
+const handleWheel = (event) => {
+    event.preventDefault;
+    cardsRef.current.scrollLeft += event.deltaY;
+}
+
+useEffect(() => {
+    cardsRef.current.addEventListener('wheel', handleWheel)
+}, [])
   return (
     <div className='title-cards'>
       <h2>Popular on Netflix</h2>
