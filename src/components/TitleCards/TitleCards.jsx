6aa52f24f34f44247fc8e88b0a53e6f342a8a284
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-29 11:44:26 -0500

    css title cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -34,7 +34,7 @@ fetch(`https://api.themoviedb.org/3/movie/${category? category : "now_playing"}?
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index) => {
-            return <Link className="card" key={index}>
+            return <Link to={`/player/${card.id}`} className="card" key={index}>
                 <img src={`https://image.tmdb.org/t/p/w500`+ card.backdrop_path} alt="" />
                 <p>{card.original_title}</p>
             </Link>
