04676af953d1b50b0f3ad51a932b8f108ecb7b1f
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-29 10:03:20 -0500

    player useEffect

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from "../../assets/back_arrow_icon.png"
 
 const Player = () => {
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: ""
+  })
+
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjNjZmFmNTk2YjhmYTg4NWQ2NjhkZThmMzRjNTc2NiIsIm5iZiI6MTc1ODIyODUyOS44NTYsInN1YiI6IjY4Y2M3MDMxZDA5MWExNGVjNDE0OTJjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2P9b2ymVIasogH6XkM4QJ1PTPGMpraMeZw7PT8Sx4M0'
+  }
+};
+
+useEffect(() => {
+  fetch('https://api.themoviedb.org/3/movie/1311031/videos?language=en-US', options)
+  .then(response => response.json())
+  .then(response => setApiData(response.results[0]))
+  .catch(err => console.error(err));
+}, [])
+
+
+
   return (
     <div className='player'>
       <img src={back_arrow_icon} alt="" />
