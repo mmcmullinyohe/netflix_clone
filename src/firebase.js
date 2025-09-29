594027b7fb2e19dd4324bfc8bae988909a60e2f1
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-29 13:28:50 -0500

    login functions

---
+++ src/firebase.js
@@ -0,0 +1,55 @@
+import { initializeApp } from "firebase/app";
+import { 
+    createUserWithEmailAndPassword, 
+    getAuth, 
+    signInWithEmailAndPassword,
+    signOut } from "firebase/auth/";
+import { 
+    addDoc, 
+    collection, 
+    getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBE59ZgI54AIM4h28ZCSq8it7ZfRV6zaM8",
+  authDomain: "netflix-clone-762ce.firebaseapp.com",
+  projectId: "netflix-clone-762ce",
+  storageBucket: "netflix-clone-762ce.firebasestorage.app",
+  messagingSenderId: "1091095012251",
+  appId: "1:1091095012251:web:30ca5c563b73d12ffeb73e"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password)
+        const user = res.user;
+        await addDoc(collection(db, "user"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const login = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password)
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const logout = () => {
+    signOut(auth);
+}
+
+export {auth, db, login, signup, logout};
